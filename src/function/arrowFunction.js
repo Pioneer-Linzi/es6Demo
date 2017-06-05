1b2ab46f233d05f46140d87dbab6f9f9b8e49335
linzi <linzi4235@gmail.com>
2017-06-05 10:11:34 +0800

    add 箭头函数

--- src/function/arrowFunction.js
+++ src/function/arrowFunction.js
@@ -39,6 +39,62 @@ var getTempItem = id => ({
  * 箭头函数可以与变量解构结合保用
  */
 
-const full = ({ firstName, lastname }) => firstName + ' ' + lastName;
+var full = ({ firstName, lastName }) => firstName + ' ' + lastName;
 
-console.log(full({ firstName: "firstName", lastName: "lastname" }))
+console.log(full({ firstName: "firstName", lastName: "lastname" }));
+
+
+const isEven = n => n % 2 == 0;
+
+
+
+/**
+ * 正常函数写法
+ */
+
+
+[1, 2, 3].map(function(x) {
+    return x * x;
+});
+
+[1, 2, 3].map(x => x * x);
+
+
+function foo() {
+    setTimeout(() => {
+        console.log('id', this.id);
+    }, 100);
+}
+
+var id = 21;
+foo.call({
+    id: 42
+});
+
+
+
+
+function Timer() {
+    this.s1 = 0;
+    this.s2 = 0;
+    constructor = () => console.log("init class");
+
+    // 箭头函数
+    setInterval(() => this.s1++, 1000);
+    // 普通函数
+    setInterval(function() {
+        this.s2++;
+    }, 1000);
+}
+
+
+
+var timer = new Timer();
+
+console.log(timer.s1);
+console.log(timer.s2);
+
+setTimeout(() => console.log('s1: ', timer.s1), 3100);
+setTimeout(() => console.log('s2: ', timer.s2), 3100);
+// s1: 3
+// s2: 0
