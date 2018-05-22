72d837a9ea2b3c6ccaf22a69cbf65d474a5f4045
linzi <linzi4235@gmail.com>
2018-05-22 16:02:53 +0800

    新增 jest 测试框架

---
+++ src/jest/jest.js
@@ -0,0 +1,17 @@
+class JestDemo{
+    constructor(){
+        this.name='linzi';
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setName(name){
+        this.name=name;
+    }
+
+
+}
+
+export default JestDemo;
